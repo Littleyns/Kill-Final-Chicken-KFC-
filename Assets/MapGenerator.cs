52e8b7bce0754adf26e30b33adfd6c92beb9649a
LittleYns <ysrhir@hotmail.fr>
2023-10-19 11:56:40 +0200

    zbi

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -4,15 +4,17 @@ public class MapGenerator : MonoBehaviour
 {
     public int mapSize = 5; // Taille de la carte (5x5, par exemple)
     public int roomSize = 29;
-    // Les modèles de salle de différentes tailles (3x3, 4x4, 5x5)
+    // Les modï¿½les de salle de diffï¿½rentes tailles (3x3, 4x4, 5x5)
     public GameObject room1Prefab;
     public GameManager gameManager;
     public GameObject[,] map;
+    public GameObject wall;
 
     void Start()
     {
         gameManager = GameObject.FindWithTag("Map").GetComponent<GameManager>();
         room1Prefab = Resources.Load("Room1") as GameObject;
+        wall = Resources.Load("Wall_D") as GameObject;
     }
 
     public void GenerateMap()
@@ -22,11 +24,38 @@ public class MapGenerator : MonoBehaviour
         {
             for (int z = 0; z < 3+ gameManager.Difficulty; z++)
             {
-                // Choisissez une salle en fonction de la difficulté
+                // Choisissez une salle en fonction de la difficultï¿½
                 GameObject room = GetRandomRoom();
 
-                // Instanciez la salle à la position (x, 0, z)
+                // Instanciez la salle ï¿½ la position (x, 0, z)
                 GameObject generatedRoom = Instantiate(room, new Vector3(x * roomSize, 0, z * roomSize), Quaternion.identity);
+
+                if(x==0)
+                {
+                    GameObject door = generatedRoom.transform.Find("Doorways").Find("Doorway_D").gameObject;
+                    GameObject generatedWall = Instantiate(wall, door.transform.position, Quaternion.identity);
+                    Destroy(door);
+                }
+                if(x == 2 + gameManager.Difficulty)
+                {
+                    GameObject door = generatedRoom.transform.Find("Doorways").Find("Doorway_B").gameObject;
+                    GameObject generatedWall = Instantiate(wall, door.transform.position, Quaternion.identity);
+                    Destroy(door);
+                }
+                if (z == 0)
+                {
+                    GameObject door = generatedRoom.transform.Find("Doorways").Find("Doorway_A").gameObject;
+                    GameObject generatedWall = Instantiate(wall, door.transform.position, Quaternion.identity);
+                    Destroy(door);
+                }
+                if (z == 2 + gameManager.Difficulty)
+                {
+                    GameObject door = generatedRoom.transform.Find("Doorways").Find("Doorway_C").gameObject;
+                    GameObject generatedWall = Instantiate(wall, door.transform.position, Quaternion.identity);
+                    Destroy(door);
+                }
+                
+
                 generatedRoom.transform.SetParent(gameObject.transform);
                 map[x,z] = generatedRoom;
 
