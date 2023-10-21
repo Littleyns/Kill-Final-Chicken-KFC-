5d34ffbf34c9a3199c3d356bfbffc6466bbf4e65
cedricSkg <cedricsockeng@gmail.com>
2023-10-21 16:09:48 +0200

    Revert "Update enemy behavior, health ans score"
    This reverts commit a87e32b8c2a915a5012b88470241a4fc09269f14.

--- Assets/EnemyBehavior.cs
+++ Assets/EnemyBehavior.cs
@@ -9,16 +9,12 @@ public class EnemyBehavior : MonoBehaviour
     public bool isDead = false;
     private GameObject player;
     private bool isPlayerClose = false;
-    public Health enemyHealth;
-    public Score points;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = this.GetComponent<Animator>();
         player = GameObject.Find("[ PLAYER SIMPLE ]");
-        enemyHealth = new Health();
-        points = new Score();
-        animator.SetBool("EnemyDeath", false);
     }
 
     // Update is called once per frame
@@ -26,27 +22,12 @@ public class EnemyBehavior : MonoBehaviour
     {
         //Atack 1 
         //Debug.Log(Vector3.Distance(this.transform.position, player.transform.position));
-        if(Vector3.Distance(this.transform.position, player.transform.position) < 0.9f){
-
-            //Attack 2 
-                //Debug.Log("Attack trigered");
-                animator.SetBool("beginAttack", true);
-                isPlayerClose = true;
-            
-        }
-        else
-            animator.SetBool("beginAttack", false);
-
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-
-        if (stateInfo.IsName("Falling Back Death") && stateInfo.normalizedTime >= 1.0f)
-        {
-            Destroy(this.gameObject);
-
-            // Update score when player kill enemy
-            points.UpdateScore();
-        }
-
+        /*if(Vector3.Distance(this.transform.position, player.transform.position) < 0.9f){
+            //Debug.Log("Attack trigered");
+            animator.SetBool("beginAttack", true);
+            isPlayerClose = true;
+        }else
+            isPlayerClose = false;*/
     }
 
      private void OnCollisionEnter(Collision collision)
@@ -59,49 +40,33 @@ public class EnemyBehavior : MonoBehaviour
             GameObject otherObject = contact.otherCollider.gameObject;
             
             // Faites quelque chose avec l'autre objet
-            //Debug.Log("ENEMY Collision avec : " + otherObject.name);
+            //Debug.Log("Collision avec : " + otherObject.name);
 
             // Animation balle
             if(otherObject.name == "Bullet(Clone)"){
                 animator.SetBool("bulletBehavior", true);
-
-                // Dimunuer la vie
-                enemyHealth.TakeDamage(10);
             }
-            if (otherObject.name == "[ PLAYER SIMPLE ]")
-            {
 
-                player.GetComponent<ThirdPersonController>().Health -= 5;
-                Debug.Log(player.GetComponent<ThirdPersonController>().Health);
+            //Attack 2 
+            if(otherObject.name == "[ PLAYER SIMPLE ]"){
+                //Debug.Log("Attack trigered");
+                animator.SetBool("beginAttack", true);
+                isPlayerClose = true;
+            }else
+                isPlayerClose = false;
             }
-        }
-        if (enemyHealth.health == 0)
-        {
-            animator.SetBool("beginAttack", false);
-            animator.SetBool("EnemyDeath", true);
-            Debug.Log("Enemy health : " + enemyHealth.health.ToString());
-        }
-
-
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
 
     }
 
     public void EndBulletBehavior(){
         animator.SetBool("bulletBehavior", false);
-        animator.SetBool("beginAttack", false);
     }
 
     public void IsAttackEnd(){
-        if(!isPlayerClose){
-            animator.SetBool("beginAttack", false);
-            //Debug.Log(isPlayerClose);
+        if(isPlayerClose){
+            //Debug.Log("Close");
+            animator.SetBool("LoopAttack", true);
         }
-            //Debug.Log("Not close");*/
+            //Debug.Log("Not close");
     }
-
-
 }
--- Assets/col.cs
+++ Assets/col.cs
@@ -1,20 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.AI.Navigation;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class col : MonoBehaviour
 {
-    public GameObject enemy;
-    public GameObject room;
-    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        enemy = Resources.Load("Enemy1") as GameObject;
-        room = GameObject.Find("Room1");
-        animator = enemy.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -29,24 +22,14 @@ public class col : MonoBehaviour
         {
             // Obtenez le GameObject de contact
             GameObject otherObject = contact.otherCollider.gameObject;
-
+            
             // Faites quelque chose avec l'autre objet
-            GameObject roomParent = otherObject.transform.parent.gameObject.transform.parent.gameObject;
-            //Debug.Log("Collision avec : " + otherObject.transform.parent.gameObject.transform.parent.gameObject);
+            Debug.Log("Collision avec : " + otherObject.name);
 
-            if(otherObject.name == "Floor_B (8)" && !GameObject.Find("Enemy1 (clone)"))
-            {
-                Instantiate(enemy, new Vector3(50, 0, 170), Quaternion.identity);
-                roomParent.AddComponent<NavMeshSurface>();
-                roomParent.GetComponent<NavMeshSurface>().BuildNavMesh();
+            if(otherObject.name == "mixamorig:LeftShoulder"){
+                
             }
         }
     }
-
-    private void OnCollisionExit(Collision other)
-    {
-
-    }
-
-
+    
 }
